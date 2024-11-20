6e171887eff985084be2a74a7738810da5129aef
VinideSouza99 <vinicius_s_lacerda@estudante.sesisenai.org.br>
2024-11-20 12:53:50 -0300

    Add files via upload

---
+++ Front/src/main.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import './index.css'
+import CadastrarEPI from './CadastroEPI'
+import Home from './Home'
+import CadastrarFuncionario from './CadastrarFuncionario'
+import Inicial from './Inicial'
+import Funcionarios from './ListaFuncionario'
+import Funcionario from './Funcionario'
+import Registro from './Registro'
+import EPIs from './ListaEPI'
+import EpiDetalhes from './EPI'
+import AtualizarFuncionario from './AtualizarFuncionario'
+import AtualizarEPI from './AtualizarEpi'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/epi/cadastro",
+    element: <CadastrarEPI />
+  },
+  {
+    path: "/funcionario/cadastro", 
+    element: <CadastrarFuncionario />,
+  },
+  {
+    path: "/principal", 
+    element: <Inicial />,
+  },
+  {
+    path: "/lista_funcionarios",
+    element: <Funcionarios/>,
+  },
+  {
+    path: "/funcionario/:id",
+    element: <Funcionario/>
+  },
+  {
+    path: "/registro",
+    element: <Registro/>,
+  },{
+    path: "/lista_EPIs",
+    element: <EPIs/>
+  },{
+    path: "/epi/:id",
+    element:<EpiDetalhes/>
+  },{
+    path: "/atualizar_funcionario/:id",
+    element: <AtualizarFuncionario/>
+  },{
+    path:"/atualizar_epi/:id",
+    element: <AtualizarEPI/>
+  }
+
+
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
